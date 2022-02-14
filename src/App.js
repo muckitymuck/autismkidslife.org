038945f90b2ac7d7e90a6023b5c7d15b15f408fe
andre <andrewgoldstein@hotmail.com>
2022-02-14 10:51:56 -0600

    add iloveu

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <br></br>
+        <br></br>
+        <br></br>
         <div class="heart"></div>
+        <p>I Love you!</p>
+        <br></br>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
