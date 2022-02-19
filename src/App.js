c8e641b614a1c8be7b0bfaa3845f3aefbe8b2a49
muckity <andrewgoldstein@hotmail.com>
2022-02-19 12:25:50 -0600

    add nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./navbar"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div class="heart"></div>
+        <Navbar />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <i class="fa-solid fa-seedling"></i>
+
+
+        </nav>
+
+    )  
+
+}
