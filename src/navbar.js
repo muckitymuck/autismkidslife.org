aca3e9c293e069dda2a2383bd638053512c65415
muckity <andrewgoldstein@hotmail.com>
2022-02-20 10:51:44 -0600

    new App.css

--- src/navbar.js
+++ src/navbar.js
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className="nav">
             <i class="fa-solid fa-seedling"></i>
-
+            <h1>AutismKidsLife</h1>
+            <ul>
+                <li>about</li>
+                <li>media</li>
+                <li>contact</li>
+            </ul>
 
         </nav>
 
