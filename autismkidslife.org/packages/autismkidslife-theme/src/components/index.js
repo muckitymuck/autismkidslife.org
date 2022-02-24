fdcc62e2627fca51a4ceccc36fcce0378261a195
muckity <andrewgoldstein@hotmail.com>
2022-02-24 06:28:58 -0600

    list and links

--- autismkidslife.org/packages/autismkidslife-theme/src/components/index.js
+++ autismkidslife.org/packages/autismkidslife-theme/src/components/index.js
@@ -1,11 +1,30 @@
 import React from "react"
 import { connect } from "frontity"
+import Link from "@frontity/components/link"
+import Switch from "@frontity/components/switch"
+import List from "./list"
 
 const Root = ( { state }) => {
+  const data = state.source.get(state.router.link)
   return (
     <>
       <h1>Frontity Workshop</h1>
       <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br />
+        <Link link="/page/2">More Posts</Link>
+        <br />
+        <Link link="/about-us">About Us</Link>
+      </nav>
+      <hr />
+      <main>
+        <Switch>
+          <List when={data.isArchive}></List>
+          <div when={data.isPost}>This is a post</div>
+          <div when={data.isPage}>This is a page</div>
+        </Switch>
+      </main>
     </>
   )
 }
