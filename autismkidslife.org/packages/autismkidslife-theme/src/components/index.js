bc9d60e819a5dfc826de1af1081707f134192fdd
muckity <andrewgoldstein@hotmail.com>
2022-02-26 07:02:44 -0600

    add styling

--- autismkidslife.org/packages/autismkidslife-theme/src/components/index.js
+++ autismkidslife.org/packages/autismkidslife-theme/src/components/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, Global, css, styled  } from "frontity"
 import Link from "@frontity/components/link"
 import Switch from "@frontity/components/switch"
 import List from "./list"
@@ -9,15 +9,31 @@ const Root = ( { state }) => {
   const data = state.source.get(state.router.link)
   return (
     <>
-      <h1>AutismKidsLife</h1>
-      <p>Current URL: {state.router.link}</p>
-      <nav>
-        <Link link="/">Home</Link>
-        <br />
-        <Link link="/page/2">More Posts</Link>
-        <br />
-        <Link link="/about-us">About Us</Link>
-      </nav>
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+          html {
+            font-family: system-ui, Verdana, Arial, sans-serif;
+          }
+        `}
+      />
+      <Header>
+          <HeaderContent>
+            <h1>AutismKidsLife</h1>
+            <p>Current URL: {state.router.link}</p>
+            <Menu>
+              <Link link="/">Home</Link>
+              <br />
+              <Link link="/page/2">More Posts</Link>
+              <br />
+              <Link link="/about-us">About Us</Link>
+            </Menu>
+          </HeaderContent>
+      </Header>
       <hr />
       <main>
         <Switch>
@@ -31,3 +47,47 @@ const Root = ( { state }) => {
 }
 
 export default connect(Root)
+
+const Header = styled.header `
+  background-color: #e5edee;
+  border-width: 0 0 8px 0;
+  border-style: solid;
+  border-color: maroon;
+
+  h1 {
+    color: #4a4a4a;
+  }
+  `
+const HeaderContent = styled.div`
+  max-width: 800px;
+  padding: 1em;
+  margin: auto;  
+  
+  img {
+    max-width: 100%;
+  }
+  h2 {
+    margin: 0.5em 0;
+  }
+  p {
+    line-height: 1.25em;
+    margin-bottom: 0.75em;
+  }
+  figcaption {
+    color: #828282;
+    font-size: 0.8em;
+    margin-bottom: 1em;
+  }  
+`
+const Menu = styled.nav`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+  & > a {
+    margin-right: 1em;
+    color: steelblue;
+    text-decoration: none;
+  }
+`
+
+  
